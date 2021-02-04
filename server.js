215d95f02e5afa4826796cd075311558557c906f
KobeHill68 <kobehill68@gmail.com>
2021-02-04 13:57:00 -0600

    updated server code

--- server.js
+++ server.js
@@ -15,12 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workouts",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
