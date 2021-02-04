9f533de43a69baa915dfa169ea590075d0426309
KobeHill68 <kobehill68@gmail.com>
2021-02-04 14:12:08 -0600

    fixed a server error

--- server.js
+++ server.js
@@ -16,13 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/workouts",
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false,
-
   }
 );
 
